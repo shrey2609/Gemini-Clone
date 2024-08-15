dcac1750a1eb126c01ae7d1397c59a2179c8d4c2
Shreyash Pandey <pandeyshreyash790@gmail.com>
2024-08-16 00:27:25 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main.jsx';
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main></Main>
+      
+    </>
+  )
+}
+
+export default App
+
